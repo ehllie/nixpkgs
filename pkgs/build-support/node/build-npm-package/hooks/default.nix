3d4804d102828349e86fa3b6bc7fbc35965aec13
Winter <winter@winter.cafe>
2023-04-28 13:00:05 -0400

    buildNpmPackage: patch npm to work around various roadblocks

--- pkgs/build-support/node/build-npm-package/hooks/default.nix
+++ pkgs/build-support/node/build-npm-package/hooks/default.nix
@@ -1,10 +1,48 @@
-{ lib, makeSetupHook, nodejs, srcOnly, buildPackages, makeWrapper }:
+{ lib, stdenvNoCC, makeSetupHook, nodejs, srcOnly, buildPackages, makeWrapper }:
 
+let
+  # We patch npm here to deal with roadblocks involving git depedencies. See
+  # front-matter in the patch, but it mainly comes down to these git
+  # dependencies must be built and pacote executes its own `npm install` process
+  # for this. We need to intercept that to run our own logic to build it
+  # similarly to the below hooks instead.
+  patchedNode = stdenvNoCC.mkDerivation {
+    name = "${nodejs.name}-patched";
+
+    src = buildPackages.nodejs;
+
+    patches = [ ./npm.patch ];
+
+    bash = lib.getExe buildPackages.bash;
+    prefetchNpmDeps = lib.getExe buildPackages.prefetch-npm-deps;
+
+    postPatch = ''
+      substituteAllInPlace lib/node_modules/npm/node_modules/pacote/lib/git.js
+
+      # We're careful here since the `node` binary contains references to this path, but we can't
+      # modify it or else things will implode on Darwin.
+      find . -type f | xargs sed -i "s|#!${buildPackages.nodejs}|#!$out|g"
+      sed -i "s|${buildPackages.nodejs}|$out|g" include/node/config.gypi
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      cp -a . "$out"
+
+      runHook postInstall
+    '';
+
+    dontFixup = true;
+  };
+in
 {
   npmConfigHook = makeSetupHook
     {
       name = "npm-config-hook";
       substitutions = {
+        inherit patchedNode;
+
         nodeSrc = srcOnly nodejs;
 
         # Specify `diff`, `jq`, and `prefetch-npm-deps` by abspath to ensure that the user's build
